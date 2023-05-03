83947fa319f4e8aaa7dff2ef803b35d8de6b705d
Zackaria Antar <antarzack@gmail.com>
2023-05-03 07:30:58 -0500

    finished type effectiveness logic and tested function for all included typing dammage included in function.

--- pokemon.js
+++ pokemon.js
@@ -1,45 +1,75 @@
-
 function calculateDamage(yourType, opponentType, attack, defense) {
-    if (yourType === 'fire' && opponentType === 'grass'){
-       return damage = 50 * (attack / defense) * 2;
-    }
-    if (yourType === 'grass' && opponentType === 'fire'){
-       return damage = 50 * (attack / defense) * .05;
-    }
-    if (yourType === 'fire' && opponentType === 'water'){
-       return damage = 50 * (attack / defense) * .5;
-    }
-    if (yourType === 'water' && opponentType === 'fire'){
-       return damage = 50 * (attack / defense) * 2;
-    }
-    if (yourType === 'fire' && opponentType === 'electric'){
-       return damage = 50 * (attack / defense) * 1;
-    }
-    if (yourType === 'electric' && opponentType === 'fire'){
-       return damage = 50 * (attack / defense) * 1;
-    }
-    if (yourType === 'water' && opponentType === 'grass'){
-       return damage = 50 * (attack / defense) * .5;
-    }
-    if (yourType === 'grass' && opponentType === 'water'){
-       return damage = 50 * (attack / defense) * 2;
-    }
-    if (yourType === 'water' && opponentType === 'electric'){
-       return damage = 50 * (attack / defense) * .5;
-    }
-    if (yourType === 'electric' && opponentType === 'water'){
-       return damage = 50 * (attack / defense) * 2;
-    }
-    if (yourType === 'grass' && opponentType === 'electric'){
-       return damage = 50 * (attack / defense) * 1;
-    }
-    if (yourType === 'electric' && opponentType === 'grass'){
-       return damage = 50 * (attack / defense) * 1;
-    }
-    if(yourType===opponentType){
-        return (damage = 50 * (attack / defense) * 1);
-    }
+	if (yourType === opponentType) {
+		return (damage = 50 * (attack / defense) * 1);
+	}
 
+	if (yourType === "fire" && opponentType === "grass") {
+		return (damage = 50 * (attack / defense) * 2);
+	}
+
+	if (yourType === "fire" && opponentType === "water") {
+		return (damage = 50 * (attack / defense) * 0.5);
+	}
+
+	if (yourType === "fire" && opponentType === "electric") {
+		return (damage = 50 * (attack / defense) * 1);
+	}
+
+	if (yourType === "grass" && opponentType === "fire") {
+		return (damage = 50 * (attack / defense) * 0.5);
+	}
+	if (yourType === "grass" && opponentType === "water") {
+		return (damage = 50 * (attack / defense) * 2);
+	}
+	if (yourType === "grass" && opponentType === "electric") {
+		return (damage = 50 * (attack / defense) * 1);
+	}
+
+	if (yourType === "water" && opponentType === "fire") {
+		return (damage = 50 * (attack / defense) * 2);
+	}
+
+	if (yourType === "water" && opponentType === "grass") {
+		return (damage = 50 * (attack / defense) * 0.5);
+	}
+
+	if (yourType === "water" && opponentType === "electric") {
+		return (damage = 50 * (attack / defense) * 0.5);
+	}
+
+	if (yourType === "electric" && opponentType === "water") {
+		return (damage = 50 * (attack / defense) * 2);
+	}
+
+	if (yourType === "electric" && opponentType === "fire") {
+		return (damage = 50 * (attack / defense) * 1);
+	}
+
+	if (yourType === "electric" && opponentType === "grass") {
+		return (damage = 50 * (attack / defense) * 1);
+	}
 }
 
-console.log(calculateDamage("fire", "water", 100, 100))
+console.log("Testing Fire:");
+console.log(calculateDamage("fire", "water", 100, 100));
+console.log(calculateDamage("fire", "grass", 100, 100));
+console.log(calculateDamage("fire", "electric", 100, 100));
+console.log(calculateDamage("fire", "fire", 100, 100));
+
+console.log("\nTesting Water:");
+console.log(calculateDamage("water", "water", 100, 100));
+console.log(calculateDamage("water", "grass", 100, 100));
+console.log(calculateDamage("water", "electric", 100, 100));
+console.log(calculateDamage("water", "fire", 100, 100));
+
+console.log("\nTesting Grass:");
+console.log(calculateDamage("grass", "grass", 100, 100));
+console.log(calculateDamage("grass", "water", 100, 100));
+console.log(calculateDamage("grass", "electric", 100, 100));
+console.log(calculateDamage("grass", "fire", 100, 100));
+
+console.log("\nTesting Electric:");
+console.log(calculateDamage("electric", "electric", 100, 100));
+console.log(calculateDamage("electric", "grass", 100, 100));
+console.log(calculateDamage("electric", "water", 100, 100));
+console.log(calculateDamage("electric", "fire", 100, 100));

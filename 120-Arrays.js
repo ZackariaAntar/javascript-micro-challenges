b0ee52e843eef75109ba2cb8f2a9bcf883774d6f
Zackaria Antar <antarzack@gmail.com>
2023-05-02 19:54:02 -0500

    finished array checkpoint

--- 120-Arrays.js
+++ 120-Arrays.js
@@ -32,3 +32,12 @@
 // ```
 
 
+function arrayCheckpoint(arr){
+    return arr[0] + arr[arr.length - 1]
+}
+
+let addingArrayEntries = arrayCheckpoint([1,2,3,4,5,6,7,8,9,10]);
+console.log(addingArrayEntries);
+
+let testingAddingArrayEntries = arrayCheckpoint([-5,2,7,3,4]);
+console.log(testingAddingArrayEntries);

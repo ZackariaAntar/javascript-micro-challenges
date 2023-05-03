24a46d149ea2634b3dafe6d833fd78a8eb68cc3b
Zackaria Antar <antarzack@gmail.com>
2023-05-02 20:40:28 -0500

    couldn't figure out how to get the loops array concatenation output on one line.

--- 130-LoopsArraysAndConcatenation.js
+++ 130-LoopsArraysAndConcatenation.js
@@ -1,7 +1,8 @@
 // # Loops, Arrays, and Concatenation Checkpoint
 // Create a function that takes two parameters. The first parameter is an array, the second parameter will be a number.
 
-// Your function should return a string that combines all of the values starting at the index of the number passed in through the end of the array.
+// Your function should return a string that combines all of the values starting at
+//  the index of the number passed in through the end of the array.
 
 // ## Examples:
 
@@ -31,3 +32,16 @@
 // ```
 // Oh! Hi There!
 // ```
+
+function loopsArrConcatCheckpoint(arr,index){
+       for (let i=index; i<arr.length; i++){
+        console.log(arr[i])
+    }
+}
+
+let testLoop = loopsArrConcatCheckpoint(['good', 'golly', 'miss', 'molly'], 2)
+console.log(testLoop);
+let testLoop1 = loopsArrConcatCheckpoint(['good', 'golly', 'miss', 'molly'], 1)
+console.log(testLoop1);
+let testLoop2 = loopsArrConcatCheckpoint(['good', 'golly', 'miss', 'molly'], 0)
+console.log(testLoop2);
